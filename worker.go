81576dc900cac3362deed4a5f338d1c22de275a2
povsister <pov@mahou-shoujo.moe>
2019-03-23 22:24:14 +0800

    Add another set of worker to deal with delayed task

--- worker.go
+++ worker.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"time"
 )
 
 type Worker struct {
@@ -31,6 +32,28 @@ func (w *Worker) start() {
 	}()
 }
 
+func (w *Worker) startDelayed() {
+	go func() {
+		for {
+			w.pool.delayedWorkerChan <- w
+			log.Printf("Delayed worker %d is waiting for task\n", w.id)
+			select {
+			case task := <-w.task:
+				log.Printf("Delayed Worker %d is handling task\n", w.id)
+				w.handleTask(*task)
+				// 等待 180 秒, 再进行下次尝试
+				time.Sleep(180 * time.Second)
+			case quitSignal := <-w.quit:
+				if quitSignal {
+					log.Printf("Delayed Worker %d stopped\n", w.id)
+					return
+				}
+
+			}
+		}
+	}()
+}
+
 func (w *Worker) handleTask(t Task) {
 	// 任务处理逻辑
 	succeed, frequent := t.process(w)
@@ -39,9 +62,9 @@ func (w *Worker) handleTask(t Task) {
 		w.pool.taskStatus.updateTaskState(t.getTaskID(), "end")
 	} else {
 		if frequent {
-			// 重新加入队列中
+			// 加入到延时队列中  该队列中的worker每180秒工作一次
 			w.pool.taskStatus.updateTaskState(t.getTaskID(), "failedFrequent")
-			w.pool.taskQueue.put(t)
+			w.pool.delayedTaskQueue.put(t)
 		} else {
 			w.pool.taskStatus.updateTaskState(t.getTaskID(), "failed")
 		}
--- workerPool.go
+++ workerPool.go
@@ -1,19 +1,26 @@
 package main
 
-import "bh3-visualNovel"
+import (
+	"bh3-visualNovel"
+)
 
 type WorkerPool struct {
-	workerChan     chan *Worker
-	workerList     []*Worker
-	taskChan       chan Task
-	taskQueue      *TaskQueue
-	taskStatus     *TaskStatus
-	libAchievement *vn.LIBAchievement
+	workerChan        chan *Worker
+	workerList        []*Worker
+	taskChan          chan Task
+	taskQueue         *TaskQueue
+	delayedWorkerChan chan *Worker
+	delayedWorkerList []*Worker
+	delayedTaskChan   chan Task
+	delayedTaskQueue  *TaskQueue
+	taskStatus        *TaskStatus
+	libAchievement    *vn.LIBAchievement
 }
 
 func initializeWorkerPool(n int) *WorkerPool {
 	// 分配队列
 	tQueue := &TaskQueue{}
+	dtQueue := &TaskQueue{}
 	// 分配任务状态
 	tS := TaskStatus{
 		task: make(map[string]taskState),
@@ -23,14 +30,19 @@ func initializeWorkerPool(n int) *WorkerPool {
 		Lib: make(map[int]vn.VnAchievements),
 	}
 	wPool = &WorkerPool{
-		workerChan:     make(chan *Worker, n),
-		workerList:     make([]*Worker, n),
-		taskChan:       make(chan Task, n),
-		taskQueue:      tQueue,
-		taskStatus:     &tS,
-		libAchievement: &lA,
+		workerChan:        make(chan *Worker, n),
+		workerList:        make([]*Worker, n),
+		taskChan:          make(chan Task, n),
+		taskQueue:         tQueue,
+		delayedWorkerChan: make(chan *Worker, n),
+		delayedWorkerList: make([]*Worker, n),
+		delayedTaskChan:   make(chan Task, n),
+		delayedTaskQueue:  dtQueue,
+		taskStatus:        &tS,
+		libAchievement:    &lA,
 	}
 	tQueue.taskChan = &wPool.taskChan
+	dtQueue.taskChan = &wPool.delayedTaskChan
 	wPool.startWorker(n)
 	wPool.startDispatch()
 	return wPool
@@ -47,12 +59,21 @@ func (wP *WorkerPool) startWorker(n int) {
 		wP.workerList = append(wP.workerList, w)
 		w.start()
 	}
+	for di := n; di < n*2; di++ {
+		dw := &Worker{
+			id:   di,
+			pool: wP,
+			task: make(chan *Task),
+			quit: make(chan bool),
+		}
+		wP.delayedWorkerList = append(wP.delayedWorkerList, dw)
+		dw.startDelayed()
+	}
 }
 
 func (wP *WorkerPool) startDispatch() {
 	go func() {
 		for {
-			//log.Println("Waiting for task to dispatch...")
 			select {
 			case task := <-wP.taskChan:
 				worker := <-wP.workerChan
@@ -63,6 +84,22 @@ func (wP *WorkerPool) startDispatch() {
 				// 判断是否需要将队列中的任务继续加入缓冲区
 				if !wP.taskQueue.isEmpty() && len(wP.taskChan) < cap(wP.taskChan) {
 					wP.taskChan <- wP.taskQueue.pop()
+				}
+			}
+		}
+	}()
+	go func() {
+		for {
+			select {
+			case task := <-wP.delayedTaskChan:
+				worker := <-wP.delayedWorkerChan
+				// 更新任务状态
+				wP.taskStatus.updateTaskState(task.getTaskID(), "start")
+				// 分配给worker
+				worker.task <- &task
+				// 判断是否需要将队列中的任务继续加入缓冲区
+				if !wP.delayedTaskQueue.isEmpty() && len(wP.delayedTaskChan) < cap(wP.delayedTaskChan) {
+					wP.delayedTaskChan <- wP.delayedTaskQueue.pop()
 				}
 			}
 		}
